cbc715ec2c7bed03a86384bfb43285a885c690e6
Kyrylo <kiriloni2k17@gmail.com>
2024-04-19 22:09:43 -0400

    writeTextFile() passing an actual value of a tree pointer not a reference for the traversal usability

--- animal.h
+++ animal.h
@@ -20,7 +20,7 @@ class BinaryTreeNode
     std::string answer;
     std::string question;
     int decideAnimal;
-    int userAnswer;
+    int usersChoice;
 
 
     bool guessAnimal(NodePtr nodePtr); // function to guess the animal
@@ -28,26 +28,31 @@ class BinaryTreeNode
     bool newRound();
     void readTextFile(NodePtr& nodePtr, std::ifstream& inFile); // function to read the text file
     int  userAnswer();
-    void userChoice(NodePtr& nodePtr, std::ofstream& outFile); // function to save the game
+    void userChoice(TreeNode& nodePtr, TreeNode& currentPtr); 
     void gameInstructions(); // function to display the game instructions
-    void writeTextFile(NodePtr& nodePtr, std::ofstream& outFile); // function to write the text file
+    void writeTextFile(NodePtr nodePtr, std::ofstream& outFile); // function to write the text file
+};
 
-
-    void BinaryTreeNode::gameInstructions() {
-        std::cout << "Welcome to the animal guessing game!" << std::endl;
-        std::cout << "Choose whether you want to start a new game or load the prior session" << std::endl;
-        std::cout << "Think of an animal and I will try to guess it." << std::endl;
-        std::cout << "If I can't guess it, you can teach me the animal and I will remember it for next time." << std::endl;
-        std::cout << "Please answer the questions with 'yes' or 'no'." << std::endl;
-        std::cout << "At the end of the game, you can choose to save the game." << std::endl;
-        std::cout << "Let's get started!" << std::endl;
-    }
-
-
-
-
-
-
-    
+void BinaryTreeNode::gameInstructions() {
+  
+   std::cout << "Choose whether you want to start a new game or load the prior session" << std::endl;
+   std::cout << "Think of an animal and I will try to guess it." << std::endl;
+   std::cout << "If I can't guess it, you can teach me the animal and I will remember it for next time." << std::endl;
+   std::cout << "Please answer the questions with 'yes' or 'no'." << std::endl;
+   std::cout << "At the end of the game, you can choose to save the game." << std::endl;
+   std::cout << "Let's get started!" << std::endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif 
