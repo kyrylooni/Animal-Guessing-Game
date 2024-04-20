ac10c1c982e9545351b58b964a65b6dbb88f7cae
Kyrylo <kiriloni2k17@gmail.com>
2024-04-19 21:42:48 -0400

    Initialized required functions in the header file. Finished gameInstruction function

--- animal.h
+++ animal.h
@@ -22,7 +22,32 @@ class BinaryTreeNode
     int decideAnimal;
     int userAnswer;
 
+
+    bool guessAnimal(NodePtr nodePtr); // function to guess the animal
+    bool makeNewQuestion(NodePtr& nodePtr); // function to make a new question
+    bool newRound();
+    void readTextFile(NodePtr& nodePtr, std::ifstream& inFile); // function to read the text file
+    int  userAnswer();
+    void userChoice(NodePtr& nodePtr, std::ofstream& outFile); // function to save the game
+    void gameInstructions(); // function to display the game instructions
+    void writeTextFile(NodePtr& nodePtr, std::ofstream& outFile); // function to write the text file
+
+
+    void BinaryTreeNode::gameInstructions() {
+        std::cout << "Welcome to the animal guessing game!" << std::endl;
+        std::cout << "Choose whether you want to start a new game or load the prior session" << std::endl;
+        std::cout << "Think of an animal and I will try to guess it." << std::endl;
+        std::cout << "If I can't guess it, you can teach me the animal and I will remember it for next time." << std::endl;
+        std::cout << "Please answer the questions with 'yes' or 'no'." << std::endl;
+        std::cout << "At the end of the game, you can choose to save the game." << std::endl;
+        std::cout << "Let's get started!" << std::endl;
+    }
+
+
+
+
+
+
+    
 }
-
-
-#endif
+#endif 
--- animalGame.cpp
+++ animalGame.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cstdlib>
 #include "animal.h"
 
 using namespace std;
@@ -11,6 +13,13 @@ int main() {
 
     // If user wants to play again after the user has guessed the animal
     bool playAgain = true;
+    ifstream inFile; // loading the game file to read the animals and questions
+    int userAnswer; // user decides to start a new game or load the game
+    ofstream outFile; // saving the game file
+    string continueGame; // user decides to continue the game or not
+    string fileName = "animalGame.txt"; // file name for the game file
+    
+
 
 
 
